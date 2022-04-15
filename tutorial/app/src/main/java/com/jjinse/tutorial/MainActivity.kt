6b57da2e811d2f3762226779f9ea7b5ac9362c4e
sejins <pengang1011@naver.com>
2022-04-15 23:37:57 +0900

    [tutorial] layout

--- tutorial/app/src/main/java/com/jjinse/tutorial/MainActivity.kt
+++ tutorial/app/src/main/java/com/jjinse/tutorial/MainActivity.kt
@@ -3,28 +3,51 @@ package com.jjinse.tutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import com.jjinse.tutorial.ui.theme.TutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard("android")
+            MessageCard(message = Message("poby", "hello!"))
         }
     }
 
+    data class Message(val author: String, val content: String)
+
 
     @Composable
-    fun MessageCard(name: String) {
-        Text(text = "Hello! $name")
+    fun MessageCard(message: Message) {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.i4),
+                contentDescription = "BMW i4",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape) // modifier 는 뷰 속성에 대한 체이닝이 가능하다.
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column {
+                Text(text = message.author)
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = message.content)
+            }
+        }
     }
 
 
@@ -32,6 +55,6 @@ class MainActivity : ComponentActivity() {
     @Preview
     @Composable
     fun PreviewMessageCard() {
-        MessageCard(name = "preview test")
+        MessageCard(message = Message("poby", "This is a test"))
     }
 }
