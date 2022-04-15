6dd0b59479432c68112d290f2070d9271ef7b78b
sejins <pengang1011@naver.com>
2022-04-15 23:03:18 +0900

    [tutorial] composable function

--- tutorial/app/src/main/java/com/jjinse/tutorial/MainActivity.kt
+++ tutorial/app/src/main/java/com/jjinse/tutorial/MainActivity.kt
@@ -10,31 +10,28 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.jjinse.tutorial.ui.theme.TutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            MessageCard("android")
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TutorialTheme {
-        Greeting("Android")
+    @Composable
+    fun MessageCard(name: String) {
+        Text(text = "Hello! $name")
+    }
+
+
+    // Preview 를 하기 위해서는 default parameter 가 반드시 존재해야하기 떄문에 다음과 같이 preview 용 composable 메서드를 생성할 수 있다.
+    @Preview
+    @Composable
+    fun PreviewMessageCard() {
+        MessageCard(name = "preview test")
     }
 }
