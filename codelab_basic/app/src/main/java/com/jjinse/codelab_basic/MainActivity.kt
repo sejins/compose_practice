c4782933d1f568e32f71b96f902eacca29979464
sejins <pengang1011@naver.com>
2022-04-24 17:08:30 +0900

    [CodelabBasic] theming

--- codelab_basic/app/src/main/java/com/jjinse/codelab_basic/MainActivity.kt
+++ codelab_basic/app/src/main/java/com/jjinse/codelab_basic/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jjinse.codelab_basic
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jjinse.codelab_basic.ui.theme.Codelab_basicTheme
@@ -106,7 +108,12 @@ fun Greeting(name: String) {
                     .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
 
             OutlinedButton(
@@ -118,16 +125,25 @@ fun Greeting(name: String) {
     }
 }
 
-@Preview
+@Preview(
+    widthDp = 320,
+    heightDp = 320
+)
 @Composable
 fun OnboardingScreenPreview() {
-    OnboardingScreen(onContinueClicked = {} )
+    Codelab_basicTheme {
+        OnboardingScreen(onContinueClicked = {} )
+    }
 }
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview(
+    name = "DefaultPreviewDark",
+    uiMode = UI_MODE_NIGHT_YES
+)
+@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Codelab_basicTheme {
-        PobyApp(list)
+        Greetings(list = list)
     }
 }
